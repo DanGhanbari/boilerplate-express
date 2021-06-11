089ad061370756f9982baff3e9df955d79afb57b
Daniel Ghanbari <danielghanbari7@gmail.com>
2021-06-11 12:32:07 +0100

    challenge 2-3

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-    console.log("Hello World");
+app.get("/", (req, res)=>{
+    const absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath)
+  })
+  console.log("Hello World");
+  
 
 
 
